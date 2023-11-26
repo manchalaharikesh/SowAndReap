234ec5a0f709022f0e43049ed2fcb1f0a97ca09a
manchalaharikesh <manchalaharikesh@gmail.com>
2023-11-27 00:57:09 +0530

    modified vercel.json commands

--- app_run_flask.py
+++ app_run_flask.py
@@ -349,4 +349,4 @@ def fertilizers():
     return render_template("result_page.html", prediction_text="Recommended Fertilizer  is {}".format(prediction[0]))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port = 8080)
